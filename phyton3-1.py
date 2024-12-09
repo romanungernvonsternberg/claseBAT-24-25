89461f31d440107c8faf7874824159e0ff5cf4e7
Tu Nombre <you@example.com>
2024-12-09 12:02:33 +0100

    IES-9D

---
+++ phyton3-1.py
@@ -0,0 +1,3 @@
+nombre = input("Dime tu nombre: ")
+apellido = input("Dime tu apellido: ")
+print("Hola, Sr. o Sra. ", apellido, ",",nombre)
---
+++ phyton3-2.py
@@ -0,0 +1,3 @@
+n1 = int(input("Dime un número: "))
+n2 = int(input("Dime otro número: "))
+print("El producto de", n1, "y", n2, "es igual a", n1*n2)
