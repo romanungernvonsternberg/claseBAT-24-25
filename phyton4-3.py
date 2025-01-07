bfd44b652749514ad9c0254db6dbee7c4c9cbecc
Tu Nombre <you@example.com>
2025-01-07 09:37:19 +0100

    IES 7_1

--- phyton4-3.py
+++ phyton4-3.py
@@ -1,7 +1,5 @@
 n1 = int(input("Dime un primer número: "))
 n2 = int(input("Dime un segundo número: "))
-if n1 == 0 :
-    print("No se puede dividir entre 0")
 if n2 == 0 :
     print("No se puede dividir entre 0")
 else :
