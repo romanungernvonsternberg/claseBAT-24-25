5dead88b6aba7b831f4951cc2684cfb01e9e9cb7
Tu Nombre <you@example.com>
2025-01-08 13:34:56 +0100

    IES8-1

---
+++ python5-1.py
@@ -0,0 +1,7 @@
+num = int(input("Introduce un número: "))
+if num > 0 :
+    print(num, "es positivo")
+elif num < 0 :
+    print(num, "es negativo")
+else :
+    print(num, "es 0")
---
+++ python5-2.py
@@ -0,0 +1,27 @@
+mes = int(input("Introduce un número entre 1 y 12: "))
+if mes == 1 :
+    print("Enero")
+elif mes == 2 :
+    print("Febrero")
+elif mes == 3 :
+    print("Marzo")
+elif mes == 4 :
+    print("Abril")
+elif mes == 5 :
+    print("Mayo")
+elif mes == 6 :
+    print("Junio")
+elif mes == 7 :
+    print("Julio")
+elif mes == 8 :
+    print("Agosto")
+elif mes == 9 :
+    print("Septiembre")
+elif mes == 10 :
+    print("Octubre")
+elif mes == 11 :
+    print("Noviembre")
+elif mes == 12 :
+    print("Diciembre")
+else :
+    print("Fuera de rango")
