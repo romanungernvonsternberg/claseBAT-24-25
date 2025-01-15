afb7188f9e603a1048b91dd13b986d57729752dc
Tu Nombre <you@example.com>
2025-01-15 13:47:17 +0100

    IES-15-1

---
+++ python6-2.py
@@ -0,0 +1,8 @@
+clave = input("Introduce la clave: ")
+contraseña = input("Introduce la contraseña: ")
+while (clave != "0987") :
+    clave = input("Clave incorrecta. Vuelve a intentarlo: ")
+print("Correcto")
+while (contraseña != "admin") :
+    contraseña = input("Código incorrecto. Vuelve a intentarlo: ")
+print("Información correcta")
